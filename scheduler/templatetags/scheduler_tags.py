f6af23b0db004fdf6a57a91fbfadeb34f0b22c76
Tiziano Pessa <tiziano.pessa@register.it>
2019-12-03 22:36:51 +0100

    Aggiunta gestione iconset

--- scheduler/templatetags/scheduler_tags.py
+++ scheduler/templatetags/scheduler_tags.py
@@ -1,12 +1,24 @@
-from ..models import Item
+from ..models import Item, Category, CATEGORY_ICONSET
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
+
+iconset = Category.objects.filter(data_type = CATEGORY_ICONSET)
 
 @register.simple_tag
 def item(item):
     if not isinstance(item, Item):
         raise TypeError('Argument must be an Item')
+    if _isIconsetItem(item):
+        return _renderIconsetItem(item)
+    else:
+        return _renderEventItem(item)
+
+def _isIconsetItem(item):
+    return iconset.filter(id = item.category.id).exists()
+
+def _renderEventItem(item):
     result = ""
     if item.start:
         result = result + item.start.strftime("%H:%M")
@@ -18,3 +30,7 @@ def item(item):
         result = result + " "
     result = result + item.description
     return result
+
+def _renderIconsetItem(item):
+    return mark_safe('<img src="/static/scheduler/' + item.description + '" />')
+    
